f48c009fb1e6cc868e2c219dd7f50bc330f5579d
pedro imbrech <elprogramadorpedro@gmail.com>
2022-03-24 19:02:37 -0300

    Preparando revisão de código

--- src/Componentes/Botao.js
+++
@@ -1,9 +0,0 @@
-function Botao(props){
-    const{texto, classe, escolherResultado} = props;
-
-    return(
-        <button className={classe} onClick={()=>escolherResultado(classe)}>{texto}</button>
-    )
-}
-
-export default Botao;
